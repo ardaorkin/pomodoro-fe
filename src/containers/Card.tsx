801325ff5f7f338eb6b21a4ec34e82c4a35dc318
Arda Örkin <ardaorkin@gmail.com>
2022-12-04 19:25:32 +0300

    hide menus unless login

--- src/containers/Card.tsx
+++ src/containers/Card.tsx
@@ -1,14 +1,17 @@
+import { useAtom } from "jotai";
 import * as React from "react";
 import TabBar from "../components/TabBar";
+import { accessTokenAtom } from "../store";
 
 interface ICardProps {
   children: React.ReactElement | React.ReactElement[];
 }
 
 const Card: React.FunctionComponent<ICardProps> = (props) => {
+  const [accessToken] = useAtom(accessTokenAtom);
   return (
     <div className="text-left space-y-12 rounded-md border-0 relative bg-red-400 flex flex-col">
-      <TabBar />
+      {accessToken && <TabBar />}
       <div
         className="p-20 border-t-2 border-red-400 shadow-xl z-10"
         style={{ width: 600, height: 750 }}
